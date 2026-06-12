84860b741d25d3f3a0e9e20198feb5e3b5012e95
Aditya Randive <adityarandive2000@gmail.com>
2026-06-13 01:56:30 +0530

    fix tpo

--- placeme-frontend/src/pages/tpo/companyForm.jsx
+++ placeme-frontend/src/pages/tpo/companyForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { tpoapi } from '../../services/tpoApi'
+import { tpoApi } from '../../services/tpoApi'
 
 const CompanyForm = () => {
   const navigate = useNavigate()
